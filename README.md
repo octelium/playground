9c521f19be5987ed6570fdc81c10ed42e1a83906
George B. <george@octelium.com>
2025-06-20 18:52:24 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Octelium Codespace Playground
+
+## What is this?
+
+This is a playground for you to install, run and manage an Octelium Cluster from within a GitHub Codespace. While we recommend you to install a demo Cluster over a cheap cloud instance (e.g. DigitalOcean, Vultr, EC2, Hetzner, etc...) or from within a Linux VM/microVM, this can be another way for you to play with Octelium without having to install it. Note the Cluster domain in our case here is going to be simply `localhost`.
+
+
+## Steps
+
+1. Run the current Repo in a Codespace via the green "Code" button on top of this page. You might probably also need to wait a minute or 2 after the Codespace is initialized since the microVM host CPUs are usually busy at startup. This has nothing to do with this repo or Octelium. It's just how Codespaces operate at initialization.
+
+2. Run the `install.sh` script as follows:
+
+```bash
+chmod 755 ./install.sh
+./install.sh
+```
+
+
+This script will take a few minutes to complete depending on the Codespace's machine type (i.e. how much RAM and vCPUs it has).
+
+3. Open a new terminal tab and start running `octeliumctl` commands:
+
+```bash
+octeliumctl get service
+#Or simply
+octeliumctl get svc
+``
---
+++ install.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+# Copyright 2025 Octelium Labs, LLC. All rights reserved. Apache 2.0 license.
+
+DOMAIN="localhost"
+VERSION="main"
+DEBIAN_FRONTEND=noninteractive
+K8S_VERSION=1.32
+PG_PASSWORD=$(openssl rand -base64 12)
+REDIS_PASSWORD=$(openssl rand -base64 12)
+
+sudo mount --make-rshared /
+sudo mkdir -p /usr/local/bin
+sudo apt-get update
+sudo apt-get install -y socat iputils-ping
+
+if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
+  export PATH="/usr/local/bin:$PATH"
+fi
+
+
+sudo rm -rf /mnt/octelium/db
+sudo mkdir -p /mnt/octelium/db
+sudo chmod -R 777 /mnt/octelium/db
+
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo cp kubectl /usr/local/bin
+sudo chmod 755 /usr/local/bin/kubectl
+
+
+export INSTALL_K3S_SKIP_START=true
+export INSTALL_K3S_SKIP_ENABLE=true
+export INSTALL_K3S_EXEC="--disable traefik"
+curl -sfL https://get.k3s.io | sh -
+
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+sudo ./get_helm.sh
+
+sudo k3s server --disable traefik --docker --write-kubeconfig-mode 644 &>/dev/null &
+
+sleep 30
+
+kubectl wait --for=condition=Ready nodes --all --timeout=600s
+
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+kubectl label nodes --all octelium.com/node=
+kubectl label nodes --all octelium.com/node-mode-controlplane=
+kubectl label nodes --all octelium.com/node-mode-dataplane=
+
+
+kubectl wait --for=condition=Ready nodes --all --timeout=600s
+
+DEVICE=$(ip route show default | ip route show default | awk '/default/ {print $5}')
+DEFAULT_LINK_ADDR=$(ip addr show "$DEVICE" | grep "inet " | awk '{print $2}' | cut -d'/' -f1)
+EXTERNAL_IP=${DEFAULT_LINK_ADDR}
+
+NODE_NAME=$(kubectl get nodes --no-headers -o jsonpath='{.items[0].metadata.name}')
+
+kubectl annotate node ${NODE_NAME} octelium.com/public-ip=${EXTERNAL_IP}
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: octelium-db-pvc
+spec:
+  resources:
+    requests:
+      storage: 5Gi
+  accessModes:
+    - ReadWriteOnce
+EOF
+
+kubectl create secret generic octelium-pg --from-literal=postgres-password=${PG_PASSWORD} --from-literal=password=${PG_PASSWORD}
+kubectl create secret generic octelium-redis --from-literal=password=${REDIS_PASSWORD}
+
+helm install --namespace kube-system octelium-multus oci://registry-1.docker.io/bitnamicharts/multus-cni --version 2.2.7 \
+    --set hostCNIBinDir=/var/lib/rancher/k3s/data/cni/ --set hostCNINetDir=/var/lib/rancher/k3s/agent/etc/cni/net.d
+
+helm install octelium-redis oci://registry-1.docker.io/bitnamicharts/redis \
+	--set auth.existingSecret=octelium-redis \
+	--set auth.existingSecretPasswordKey=password \
+	--set architecture=standalone \
+	--set master.persistence.enabled=false \
+	--set standalone.persistence.enabled=false \
+  --set networkPolicy.enabled=false --version 20.8.0
+
+helm install --wait --timeout 30m0s octelium-pg oci://registry-1.docker.io/bitnamicharts/postgresql \
+	--set primary.persistence.existingClaim=octelium-db-pvc \
+	--set global.postgresql.auth.existingSecret=octelium-pg \
+	--set global.postgresql.auth.database=octelium \
+	--set global.postgresql.auth.username=octelium \
+  --set primary.networkPolicy.enabled=false --version 16.4.14
+
+curl -fsSL https://octelium.com/install.sh | bash
+
+
+export OCTELIUM_REGION_EXTERNAL_IP=${EXTERNAL_IP}
+export OCTELIUM_AUTH_TOKEN_SAVE_PATH="/tmp/octelium-auth-token"
+octops init ${DOMAIN} --version ${VERSION} --bootstrap - <<EOF
+spec:
+  primaryStorage:
+    postgresql:
+      username: octelium
+      password: ${PG_PASSWORD}
+      host: octelium-pg-postgresql.default.svc
+      database: octelium
+      port: 5432
+  secondaryStorage:
+    redis:
+      password: ${REDIS_PASSWORD}
+      host: octelium-redis-master.default.svc
+      port: 6379
+EOF
+
+kubectl wait --for=condition=available deployment/svc-default-octelium-api --namespace octelium --timeout=600s
+kubectl wait --for=condition=available deployment/svc-auth-octelium-api --namespace octelium --timeout=600s
+kubectl wait --for=condition=available deployment/octelium-ingress-dataplane --namespace octelium --timeout=600s
+kubectl wait --for=condition=available deployment/octelium-ingress --namespace octelium --timeout=600s
+
+
+AUTH_TOKEN=$(cat $OCTELIUM_AUTH_TOKEN_SAVE_PATH)
+
+export OCTELIUM_DEV=true
+export OCTELIUM_DOMAIN="localhost"
+
+sleep 3
+
+octelium login --domain localhost --auth-token $AUTH_TOKEN
+
+echo "export OCTELIUM_DEV=\"$OCTELIUM_DEV\"" >> ~/.bashrc
+echo "export OCTELIUM_DOMAIN=\"$OCTELIUM_DOMAIN\"" >> ~/.bashrc
+echo "export KUBECONFIG=\"$KUBECONFIG\"" >> ~/.bashrc
+
+if [ -f ~/.zshrc ]; then
+  echo "export OCTELIUM_DEV=\"$OCTELIUM_DEV\"" >> ~/.zshrc
+  echo "export OCTELIUM_DOMAIN=\"$OCTELIUM_DOMAIN\"" >> ~/.zshrc
+  echo "export KUBECONFIG=\"$KUBECONFIG\"" >> ~/.zshrc
+fi
+
+source ~/.bashrc
